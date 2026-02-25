79f46f737bbb43d792b20f324829e5508ab76b9c
edu7k001 <edu7k001@gmail.com>
2026-02-24 22:15:14 -0300

    Update cashinpay.js

--- config/cashinpay.js
+++ config/cashinpay.js
@@ -4,15 +4,15 @@ const CASHINPAY_API_KEY = process.env.CASHINPAY_API_KEY;
 const CASHINPAY_BASE_URL = 'https://api.cashinpaybr.com/api/v1';
 
 if (!CASHINPAY_API_KEY) {
-  throw new Error('CASHINPAY_API_KEY não definida nas variáveis de ambiente');
+    throw new Error('CASHINPAY_API_KEY não definida nas variáveis de ambiente');
 }
 
 const cashinpayApi = axios.create({
-  baseURL: CASHINPAY_BASE_URL,
-  headers: {
-    'Authorization': `Bearer ${CASHINPAY_API_KEY}`,
-    'Content-Type': 'application/json',
-  },
+    baseURL: CASHINPAY_BASE_URL,
+    headers: {
+        'Authorization': `Bearer ${CASHINPAY_API_KEY}`,
+        'Content-Type': 'application/json',
+    },
 });
 
 module.exports = cashinpayApi;
